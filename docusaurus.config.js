69760bae88452ca45386fcc6fbee4693975a49f9
Luke Van <lukevrensburg@gmail.com>
2025-06-01 19:39:46 +0200

    fixed error

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -50,8 +50,7 @@ const config = {
           sidebarPath: "./sidebars.js",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/lukevanlukevan/lv-tools-docs/tree/main/docs/,
+          editUrl: "https://github.com/lukevanlukevan/lv-tools-docs/tree/main/",
         },
         // blog: {
         //   showReadingTime: true,
